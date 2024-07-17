9d214e78402e028c9d38b2b96dbd95562a50e29c
欧子 <ouzi7997@163.com>
2024-07-18 01:40:02 +0800

    样式

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap'
 import '../styles/index.css'
 import './login'
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import '../scss/login.css'
 import '../scss/font.css'
 import 'virtual:svg-icons-register';
