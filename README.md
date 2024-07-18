6308716a62faedf07339f8fca4201a7f09aebefb
欧子 <ouzi7997@163.com>
2024-07-18 23:34:05 +0800

    放弃vue-element-template 尝试引入vue-element-plus-admin

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ouzicode
 - Laravel 10
 - Element Plus
+- vue-element-plus-admin
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
 import './bootstrap'
-import '../styles/index.css'
-import './login'
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-import '../scss/login.css'
-import '../scss/font.css'
-import 'virtual:svg-icons-register';
+import.meta.glob([
+    '../assets/imgs/**',
+    '../fonts/**',
+]);
