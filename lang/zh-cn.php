9e0f5c54ae904ea5490f43868282de6ac390b8ae
欧子 <ouzi7997@163.com>
2024-09-07 01:48:15 +0800

    中文语言包 文案配置

---
+++ lang/zh-cn.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'common'=>[
+        'login'=>'登录',
+        'mainPage'=>'首页',
+        'list'=>'列表',
+        'moreDelete'=>'批量删除',
+        'add'=>'添加',
+        'search'=>'查询',
+        'input'=>'请输入',
+        'edit'=>'修改',
+        'delete'=>'删除',
+    ],
+    'title'=>[
+
+    ]
+];
