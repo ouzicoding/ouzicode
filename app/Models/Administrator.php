d47a415e17f7a7aba59ca658234cea8da06e6f2a
欧子 <ouzi7997@163.com>
2024-09-06 18:49:22 +0800

    ORM 模型

--- app/Models/Administrator.php
+++ app/Models/Administrator.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Eloquent模块，是一个对象关系映射(ORM)
+ * 表->模型
+ */
 class Administrator extends Model
 {
     use HasFactory;
+
 
 
 }
